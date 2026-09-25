bf938e6ea57e20730f8d239cc499ab4b02e962b5
shaleh2001 <shalehirawan11@gmail.com>
2026-09-25 07:52:22 +0700

    Refactor motorcycle data in app.js
    Updated product names and removed down payment, installment, and tenor details for several motorcycle entries.

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const products = [
 /* ================= MAXI ================= */
 
 {
-  name: "TMAX TECH MAX",
+  name: "TMAX",
   category: "maxi",
   price: "Rp 455.200.000",
-  dp: "Rp 136.000.00",
-  installment: "RP 11.522.000",
-  tenor: "35",
   image: "2026012109464595907G85940.png"
 },
 
@@ -18,9 +15,6 @@ const products = [
   name: "XMAX 250",
   category: "maxi",
   price: "Rp 70.490.000",
-  dp: "Rp 15.600.000",
-  installment: "Rp 2.601.000"",
-  tenor: "29",
   image: "2026011410411324315K95315.png"
 },
 
@@ -28,9 +22,6 @@ const products = [
   name: 'NMAX "TURBO"',
   category: "maxi",
   price: "Rp 39.460.000",
-  dp: "Rp 2.500.000",
-  installment: "Rp 1.623.000",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/new_product_model_image/2025111019120360785H74074.png"
 },
 
@@ -38,9 +29,6 @@ const products = [
   name: "AEROX ALPHA",
   category: "maxi",
   price: "Rp 31.285.000",
-  dp: "Rp 1.500.000",
-  installment: "1300.000",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/new_product_model_image/2026020515031335833N50491.png"
 },
 
@@ -48,9 +36,6 @@ const products = [
   name: "LEXi LX 155",
   category: "maxi",
   price: "Rp 28.220.000",
-  dp: "Rp 1.300.000",
-  installment: "Rp 1.179.000",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/new_product_model_image/2026012101353067003N34382.png"
 },
 
@@ -58,9 +43,6 @@ const products = [
   name: "NMAX NEO S",
   category: "maxi",
   price: "Rp 35.590.000",
-  dp: "Rp 1.700.000",
-  installment: "Rp 1.471.000",
-  tenor: "35",
   image: "2025111019194058600Y79045.png"
 },
 
@@ -68,9 +50,6 @@ const products = [
   name: "AEROX 155",
   category: "maxi",
   price: "Rp 29.705.000",
-  dp: "Rp 1.300.000",
-  installment: "Rp 1.240.000",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/new_product_model_image/2023041415280563814D66821.png"
 },
 
@@ -80,9 +59,6 @@ const products = [
   name: "GRAND FILANO",
   category: "classy",
   price: "Rp 28.960.000",
-  dp: "Rp 1.200.000",
-  installment: "Rp 1.211.000",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2026012314263490145D59534.png"
 },
 
@@ -90,9 +66,6 @@ const products = [
   name: "FAZZIO Neo",
   category: "classy",
   price: "Rp 24.650.000",
-  dp: "Rp 1.000.000",
-  installment: "Rp 1.040.000",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2026012315293027645K32651.png"
 },
 
@@ -102,19 +75,13 @@ const products = [
   name: "GEAR ULTIMA",
   category: "matic",
   price: "Rp 20.240.000",
-  dp: "Rp 800.000",
-  installment: "Rp 874.000",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/202604021124218894J.png"
 },
 
 {
-  name: "GEAR ultima Solid",
+  name: "GEAR 125",
   category: "matic",
-  price: "Rp 20.870.000",
-  dp: "Rp 1.000.000",
-  installment: "Rp 892.000",
-  tenor: "35",
+  price: "Rp 20.450.000",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2024031507431441417B35284.png"
 },
 
@@ -122,9 +89,6 @@ const products = [
   name: "FREEGO 125",
   category: "matic",
   price: "Rp 23.205.000",
-  dp: "Rp 1.000.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/202506091443425485Y97917.png"
 },
 
@@ -132,9 +96,6 @@ const products = [
   name: "X-RIDE 125",
   category: "matic",
   price: "Rp 21.435.000",
-  dp: "Rp 1.300.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2025070409402252373C51406.png"
 },
 
@@ -142,9 +103,6 @@ const products = [
   name: "MIO M3 125",
   category: "matic",
   price: "Rp 19.200.000",
-  dp: "Rp 1.000.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2025051913533472814Q20921.png"
 },
 
@@ -152,9 +110,6 @@ const products = [
   name: "FINO 125",
   category: "matic",
   price: "Rp 20.900.000",
-  dp: "Rp 1.300.000i",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/202302210939153521B87826.png"
 },
 
@@ -164,9 +119,6 @@ const products = [
   name: "XSR 155",
   category: "sport",
   price: "Rp 40.845.000",
-  dp: "Rp 2.000.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2025012015110954865E18335.png"
 },
 
@@ -174,9 +126,6 @@ const products = [
   name: "R15 CONNECT",
   category: "sport",
   price: "Rp 42.975.000",
-  dp: "Rp 4.000.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2024072510414155568H76580.png"
 },
 
@@ -184,9 +133,6 @@ const products = [
   name: "R25",
   category: "sport",
   price: "Rp 76.750.000",
-  dp: "Rp 14.600.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2025012015411993760O36900.png"
 },
 
@@ -194,9 +140,6 @@ const products = [
   name: "MT-25",
   category: "sport",
   price: "Rp 67.150.000",
-  dp: "Rp 14.100.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2025012015192887371O6508.png"
 },
 
@@ -204,9 +147,6 @@ const products = [
   name: "MT-15",
   category: "sport",
   price: "Rp 41.445.000",
-  dp: "Rp 7.900.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2025012015144954158N49912.png"
 },
 
@@ -214,9 +154,6 @@ const products = [
   name: "VIXION 155",
   category: "sport",
   price: "Rp 31.965.000",
-  dp: "Rp 3.300.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/new_product_model_image/2022011109442589529O58043.png"
 },
 
@@ -226,9 +163,6 @@ const products = [
   name: "WR155R",
   category: "off-road",
   price: "Rp 41.675.000",
-  dp: "Rp 4.000.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2026012216283431324H51389.png"
 },
 
@@ -236,9 +170,6 @@ const products = [
   name: "YZ125X",
   category: "off-road",
   price: "Rp 100.200.000",
-  dp: "Rp 15.800.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/202606031052219193A.png"
 },
 
@@ -246,9 +177,6 @@ const products = [
   name: "YZ250X",
   category: "off-road",
   price: "Rp 132.500.000",
-  dp: "Rp 20.300.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/202606031053343044T.png"
 },
 
@@ -256,9 +184,6 @@ const products = [
   name: "YZ250FX",
   category: "off-road",
   price: "Rp 140.500.000",
-  dp: "Rp 21.300.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/202606031050479381F.png"
 },
 
@@ -268,9 +193,6 @@ const products = [
   name: "MX KING 150",
   category: "moped",
   price: "Rp 29.379.000",
-  dp: "Rp 2.600.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/202606190939128383S.png"
 },
 
@@ -278,9 +200,6 @@ const products = [
   name: "JUPITER Z1",
   category: "moped",
   price: "Rp 22.771.000",
-  dp: "Rp 1.850.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2025061108344332282O17129.png"
 },
 
@@ -288,11 +207,9 @@ const products = [
   name: "VEGA FORCE",
   category: "moped",
   price: "Rp 20.445.000",
-  dp: "Rp 1.500.000",
-  installment: "Hubungi Kami",
-  tenor: "35",
   image: "https://www.yamaha-motor.co.id/uploads/products/featured_image/2023060510573539478H29934.png"
 }
+  
 
 ];
 
